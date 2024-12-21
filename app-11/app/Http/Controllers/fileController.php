d4cbd5d1f89b1545d2a396dd016ed2ec06c186bf
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-12-21 18:52:30 +0600

    route

--- app-11/app/Http/Controllers/fileController.php
+++ app-11/app/Http/Controllers/fileController.php
@@ -1,26 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
-// use Illuminate\Http\Request;
+
+
+use Illuminate\Http\Request;
 
 class fileController extends Controller
 {
-   function download()
+   function download(Request $request)
     {
         return response("This Is Download File");
     }
 
-   function downloadId($invoiceId){
+   function downloadId(Request $request,$invoiceId){
         return response("This Is Download File:{$invoiceId}");
     }
 
 
-    function downloadIdWithFileType($FileType){
+    function downloadIdWithFileType(Request $request,$FileType){
         return response("This Is Download png File:{$FileType}");
     }
 
     function error(){
         return response("Please Your Download Id Provide",404);
     }
-   
+
 }
--- app-11/routes/web.php
+++ app-11/routes/web.php
@@ -14,5 +14,5 @@ Route::get('/about',[aboutController::class,'about']);
 Route::get('/about/me',[aboutController::class,'aboutMe']);
 Route::get('/download',action: [fileController::class,'download']);
 Route::get('/download/invoice/{invoiceId}',action: [fileController::class,'downloadId']);
-Route::get('/download/invoice/{invoiceId}/type/{FileType}',action: [fileController::class,'downloadIdWithFileType']);
+Route::get('/download/invoice/{invoiceId}/type/{FileType?}',action: [fileController::class,'downloadIdWithFileType']);
 Route::get('/download/invoice', action: [fileController::class, 'error']);
