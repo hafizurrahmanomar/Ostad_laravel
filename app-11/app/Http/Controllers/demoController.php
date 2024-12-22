3252111168b98c0466526aeb9405651177d6e89a
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-12-22 19:47:12 +0600

    Laravel Request and response

--- app-11/app/Http/Controllers/demoController.php
+++ app-11/app/Http/Controllers/demoController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 
 class demoController extends Controller
 {
-    function demo1(){
+
+    //Laravel Response
+    function demo1()
+    {
         // return 'Hello world! ';
         // return  100;
         // return true;
@@ -21,10 +24,72 @@ class demoController extends Controller
 
     }
 
-    function demo2(){
-    return view('demo2');
-            }
 
+    //Laravel methods
+    function demo2(Request $request)
+    {
+        return "Hello world!";
+
+
+    }
+    //Laravel query   
+    function demo7(Request $request)
+    {
+
+        $qry = $request->query();
+        return $qry;
+
+    }
+
+
+
+
+    function demo8(Request $request)
+    {
+
+        $name = $request->name;
+        $age = $request->age;
+        return $name . " " . $age;
+    }
+
+    //Jason Body with input()
+    function demo9(Request $request)
+    {
+        $jasonData = $request->input();
+        return $jasonData;
+    }
+
+    //Jason Header
+    function demo10(Request $request)
+    {
+        $Header = $request->header();
+        return $Header;
+    }
+
+    //Form Data
+    function demo11(Request $request)
+    {
+        $key1 = $request->input('key1');
+        $key2 = $request->input('key2');
+        $key3 = $request->input('key3');
+        return $key1 . " " . $key2;
+    }
+
+    
+    function demo12(Request $request)
+    {
+        $myFile = $request->input('myFile');
+        $myFile->move(public_path('uploads'),$myFile->getClientOriginalName());
+
+        return "File uploaded successfully";
+    }
+
+
+    //Request IP Address Data
+    function demo13(Request $request)
+    {
+        return $request->ip();
+    }
 
 
 }
--- app-11/bootstrap/app.php
+++ app-11/bootstrap/app.php
@@ -11,8 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        //CSRF property switch off for practice purpose
+        $middleware->validateCsrfTokens([
+            
+            '*',
+        ]);
     })
+
+    
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
