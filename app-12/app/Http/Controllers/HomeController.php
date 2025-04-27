60c1c55469520f91b8c70a37956c4e91dba45276
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2025-04-27 15:31:40 +0600

    Laravel_12_install_HomeController_create

---
+++ app-12/app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(Request $request)
+    { {
+            //return view('home.index');
+            return response("This is the home page");
+        }
+
+
+    }
+}
